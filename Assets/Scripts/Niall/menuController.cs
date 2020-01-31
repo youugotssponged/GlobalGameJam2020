6ce15c2a9fb377f3528711163dd24cf80193b4d1
Niall <23546468@edgehill.ac.uk>
2020-01-31 18:53:23 +0000

    Main menu added
    + Play button
      - Loads new scene (will be game start scene)
    + Instructions button
      - Switches to text explaining instructions
    + Exit game
      - Closes the application (not in editor)

---
+++ Assets/Scripts/Niall/menuController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuController : MonoBehaviour
+{
+    public GameObject mainMenuObject;
+    public GameObject instructionsObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playButtonPressed()
+    {
+        SceneManager.LoadScene(1); //change the number to main game scene number in build order
+        Debug.Log("Play");
+    }
+
+    public void instructionsButtonPressed()
+    {
+        mainMenuObject.SetActive(false);
+        instructionsObject.SetActive(true);
+        Debug.Log("Instructions");
+    }
+
+    public void exitButtonPressed()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+
+    public void returnToMenu()
+    {
+        mainMenuObject.SetActive(true);
+        instructionsObject.SetActive(false);
+    }
+
+}
