63d3bf89f1d8c52e420604dbcdfa3a98a93aa7f3
T-Man80 <45274689+T-Man80@users.noreply.github.com>
2020-02-01 22:56:39 +0000

    Cutscene

---
+++ Assets/Scripts/Tony/MeteorMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorMover : MonoBehaviour
+{
+    [SerializeField] private Transform start;
+    [SerializeField] private float speed;
+
+    private float randomX;
+    private float randomY;
+    [SerializeField] private int amount = 0;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(amount <= 2)
+        {
+            randomX = Random.Range(0f, 25f);
+            randomY = Random.Range(13f, 26f);
+
+            amount++;
+
+            Debug.Log("HIT!");
+
+            start.position = new Vector3(randomX, randomY, start.position.z);
+
+            transform.position = start.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, -speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Tony/UFORotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFORotator : MonoBehaviour
+{
+    [SerializeField] private GameObject UFO;
+    [SerializeField] private float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UFO.transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}
