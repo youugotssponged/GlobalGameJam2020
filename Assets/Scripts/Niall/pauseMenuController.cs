f027c8dd23ce58fbcd86554962cbfcdde08a183f
Niall <23546468@edgehill.ac.uk>
2020-02-01 00:35:17 +0000

    Added pause menu
    + When escape is pressed during the game, it freezes and the pause menu comes up
      - Resume button unfreezes the game and hides the pause menu
      - Settings button does nothing yet
      - Return to menu button loads the main menu scene

---
+++ Assets/Scripts/Niall/pauseMenuController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenuController : MonoBehaviour
+{
+    public GameObject pauseMenuObject;
+    bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (paused) //if the game is already paused, resume the game
+            {
+                paused = false;
+                resumeGame();
+            } else //if the game isn't paused, pause the game
+            {
+                paused = true;
+                pauseGame();
+            }
+        }
+    }
+
+    void pauseGame()
+    {
+        Time.timeScale = 0f; //set the timescale to 0, freezing the game
+        pauseMenuObject.SetActive(true); //show the pause menu
+    }
+
+    void resumeGame()
+    {
+        Time.timeScale = 1f; //set the timescale to 1, unfreezing the game
+        pauseMenuObject.SetActive(false); //hide the pause menu
+    }
+
+    public void resumeButtonPressed()
+    {
+        //resume the game
+        resumeGame();
+    }
+
+    public void settingsButtonPressed()
+    {
+        //open the settings menu
+    }
+
+    public void returnToMenuButtonPressed()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
